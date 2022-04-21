55318dd6676b663b549915f79d9691c152be171e
Alex Hector Astuhuaman Delgado <alhexanderad@gmail.com>
2022-04-20 23:18:30 -0500

    Se realizo footer js

--- website/main.js
+++ website/main.js
@@ -1,4 +1,15 @@
+/**Para menu burger */
+const navbar = document.querySelector('.header .navbar');
+const menubutton = document.querySelector('.header .menu');
+
+menubutton.addEventListener('click', () =>{
+  navbar.classList.toggle('show');
+})
+
+/**Navbar */
 document.onscroll = () =>{
+  navbar.classList.remove('show');
+  
   if (window.scrollY > 0){
   document.querySelector('.header').classList.add('active');
 }
